73c9ea34f862a346fa878e50cc04c35e45e86d4a
Richie <43894274+ric99cs@users.noreply.github.com>
2021-02-22 10:02:17 +0100

    Update setting.php

--- include/setting.php
+++ include/setting.php
@@ -1,9 +1,9 @@
 <?php
     header('Content-Type: text/html; charset=UTF8');
     $host = "localhost";
-    $user = "web430";
-    $pass = "sDCv6F49";
-    $db = "usr_web430_3";
+    $user = "xxxxxxx";
+    $pass = "xxxxxxx";
+    $db = "xxxxxxx";
     $recipient="e.richter@schott.tv, h.schott@schott.tv";  
     $header="Verfügbarkeitscheck - Suchanfrage";
     $datum=date("d.m.y");
